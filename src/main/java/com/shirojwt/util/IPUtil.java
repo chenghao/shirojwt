70683e13fa6699c538b4cb3431374a95c2cf469b
gaunt <gaunt@163.com>
2019-10-23 15:44:54 +0800

    日志新增ip显示

--- src/main/java/com/shirojwt/util/IPUtil.java
+++ src/main/java/com/shirojwt/util/IPUtil.java
@@ -19,7 +19,9 @@ public class IPUtil {
      */
     public static String getIpByReq() {
         HttpServletRequest request = HttpContextUtils.getHttpServletRequest();
-
+        return getIpByReq(request);
+    }
+    public static String getIpByReq(HttpServletRequest request) {
         String ip = request.getHeader("x-forwarded-for");
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
